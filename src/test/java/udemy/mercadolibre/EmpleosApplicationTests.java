e221810646d9e9da5175f2cd4be25b87815c25fd
JulianaOrtegaMeli <juliana.ortega@mercadolibre.com.co>
2023-04-09 22:29:39 -0500

    project scaffolding

---
+++ src/test/java/udemy/mercadolibre/EmpleosApplicationTests.java
@@ -0,0 +1,13 @@
+package udemy.mercadolibre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpleosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
